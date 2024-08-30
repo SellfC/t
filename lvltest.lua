f8553a887dc1b4b9b8fbc613ea49a428edaea63f
SellfC <110660214+SellfC@users.noreply.github.com>
2024-08-31 00:47:43 +0300

    Update lvltest.lua

--- lvltest.lua
+++ lvltest.lua
@@ -1,1 +1,76 @@
-print("lol")
+local Player = game:GetService("Players").LocalPlayer
+if Player.Name == "Wobblebread6577" then
+getgenv().PrivateServerCode = "SQlY2VzjTi"
+elseif Player.Name == "kultnogve" then
+getgenv().PrivateServerCode = "ODmcKl4ZQl" 
+elseif Player.Name == "esplorahoteld" then
+getgenv().PrivateServerCode = "Q1u9b5O3M0" 
+elseif Player.Name == "faloni5v" then
+getgenv().PrivateServerCode = "HAY3bKUdQ6" 
+elseif Player.Name == "2zmbgm" then
+getgenv().PrivateServerCode = "a7cZoyGucM" 
+elseif Player.Name == "Krancho5637" then
+getgenv().PrivateServerCode = "0YBKc0vxSq" 
+elseif Player.Name == "Hronba3672" then
+getgenv().PrivateServerCode = "FPARuJreaK" 
+elseif Player.Name == "Shadofu0814" then
+getgenv().PrivateServerCode = "E20kF5imgP" 
+elseif Player.Name == "Rexco0773" then
+getgenv().PrivateServerCode = "FvJuPCILTU" 
+elseif Player.Name == "Crutfu4619" then
+getgenv().PrivateServerCode = "DBl15XgrtF" 
+elseif Player.Name == "Gracflu1044" then
+getgenv().PrivateServerCode = "lIxyvt3OzY" 
+elseif Player.Name == "Echobyte1546" then
+getgenv().PrivateServerCode = "EPn6eNn90g" 
+elseif Player.Name == "openteacherbd" then
+getgenv().PrivateServerCode = "QdzU2NELxo"
+end
+getgenv().TweenSpeed = 500 -- max 500
+getgenv().GunStats = true -- Auto Gun Stats
+
+getgenv().AutoGetHaki = true  -- Auto Get Haki
+getgenv().AutoGetGeppo = true -- Auto Get Geppo
+getgenv().AutoGetWorldScroll = true -- Auto Get World Scroll
+getgenv().AutoGetTradableItems = true -- Auto Get Tradable Items
+getgenv().AutoGetStaminaDrinks = true -- Auto Get Stamina Drinks
+getgenv().AutoGoToSecondSea = true -- Auto Go To Second Sea
+
+getgenv().FirstSea = "Town of Beginnings" -- or other first sea island
+-- -- FIRST SEA NAME ISLANDS:
+-- "Town of Beginnings"
+-- "Sandora"
+-- "Shell's Town"
+-- "Zou Island"
+-- "Baratie"
+-- "Orange Town"
+-- "Roca Island"
+-- "Colosseum"
+-- "Sphinx Island"
+-- "Arlong Park"
+-- "Land of The Sky"
+-- "Gravito's Fort"
+-- "Fishman Island"
+-- "Marine Base"
+-- "Reverse Mountain"
+getgenv().SetSpawnFirstSea = false -- Auto Set Spawn on First Sea Island
+
+getgenv().SecondSea = "Impel Base" -- or other second sea island
+-- -- SECOND SEA NAME ISLANDS:
+-- "Desert Kingdom"
+-- "Colosseum of Arc"
+-- "Thriller Bark"
+-- "Foro Island"
+-- "Rose Kingdom"
+-- "Impel Base"
+-- "Sett's Arena"
+getgenv().SetSpawnSecondSea = true -- Auto Set Spawn on Second Sea Island (SET TRUE AUTO GO TO SECOND SEA)
+
+ -- Put Your Private Server Code
+getgenv().WebhookURL = "Webhook" -- Put Your Webhook
+
+getgenv().AutoFastMode = true -- Auto Fast Mode
+getgenv().Disable3DRender = false -- Auto Disable 3DRender
+
+script_key="tYiUwQfUGdzMfmgurRGimpquWgCubtgm";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3d19f8ff99e66b11847e169e9ba3b55d.lua"))()
